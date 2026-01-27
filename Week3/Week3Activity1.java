46703a4bb530c48a4729c70316d8bcde67bde9b8
yvonnevalerie <yvonnevalerie.jacalan@lorma.edu>
2026-01-27 11:18:42 +0800

    Act1WEEK3

--- Week3/Week3Activity1.java
+++ Week3/Week3Activity1.java
@@ -1,36 +1,49 @@
+import java.util.Scanner;
+
 public class Week3Activity1 {
-    public static void main (String[] args){
+
+    public static void main(String[] args) {
+        //Problem 1
         Scanner sc = new Scanner(System.in);
-        
-        double[][]matrix = new double [3][4];
+        double[][] matrix = new double[3][4];
+        double[][] matrix2 = new double[4][4];
         System.out.println("Enter a 3-by-4 matrix row by row:");
-            for(int i = 0; i < matrix.length; i++ ){
-                for(int j = 0; j < matrix[i].length; j++){
-                    matrix[i][j] = sc.nextDouble();
-                    
-                }
-                
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                matrix[i][j] = sc.nextDouble();
+            }
         }
-        System.out.println("Sum of the elements at column" + columnIndex + "is" + sumColumn(matrix, 0));
-
-    public static double sumColumn(double[][] m, int columnIndex){
-            double sum = 0;
-            for (int row = 0; row < natrix.length; row++){
-                sum += m[i][columnIndex];
-
-            }
-                sum += m[i][columnIndex];
-
-        
-        
-
-                }
-                return sum;
-            }
-            public static double sumMajorDiagonal(double [][] matrix){
-
-            }
-
-
+        for (int col = 0; col < 4; col++) {
+            System.out.println("Sum of the elements at column " + col +
+                    " is " + sumColumn(matrix, col));
         }
+        //Problem 2
+        System.out.println("\nEnter a 4-by-4 matrix row by row:");
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                matrix2[i][j] = sc.nextDouble();
+            }
+        }
+        System.out.println("Sum of the elements in the major diagonal is "
+                + sumMajorDiagonal(matrix2));
+
+    public static double sumColumn(double[][] m, int columnIndex) {
+        double sum = 0;
+        for (int i = 0; i < m.length; i++) {
+            sum += m[i][columnIndex];
+        }
+        return sum;
+    }
+
+    public static double sumMajorDiagonal(double[][] m) {
+        double sum = 0;
+        for (int i = 0; i < m.length; i++) {
+            sum += m[i][i];
+        }
+        return sum;
+    }
+
     
+        }
+
+}
