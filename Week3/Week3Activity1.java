0da1dfb453c2a77371ea552cf27f8917e9bbddde
yvonnevalerie <yvonnevalerie.jacalan@lorma.edu>
2026-01-27 12:32:01 +0800

    week3Act1

--- Week3/Week3Activity1.java
+++ Week3/Week3Activity1.java
@@ -3,32 +3,41 @@ import java.util.Scanner;
 public class Week3Activity1 {
 
     public static void main(String[] args) {
-        //Problem 1
+
         Scanner sc = new Scanner(System.in);
-        
+
+        // Problem 1
         double[][] matrix = new double[3][4];
-        double[][] matrix2 = new double[4][4];
-        
+
         System.out.println("Enter a 3-by-4 matrix row by row:");
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 4; j++) {
                 matrix[i][j] = sc.nextDouble();
             }
         }
+
         for (int col = 0; col < 4; col++) {
-            System.out.println("Sum of the elements at column " + col +
-                    " is " + sumColumn(matrix, col));
+            System.out.println(
+                "Sum of the elements at column " + col + " is " + sumColumn(matrix, col)
+            );
         }
-        //Problem 2
-        System.out.println("\nEnter a 4-by-4 matrix row by row:");
+
+        // Problem 2
+        double[][] matrix2 = new double[4][4];
+
+        System.out.println("Enter a 4-by-4 matrix row by row:");
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 matrix2[i][j] = sc.nextDouble();
             }
         }
-        System.out.println("Sum of the elements in the major diagonal is "
-                + sumMajorDiagonal(matrix2));
 
+        System.out.println(
+            "Sum of the elements in the major diagonal is " + sumMajorDiagonal(matrix2)
+        );
+    }
+
+    // Method for Problem 1
     public static double sumColumn(double[][] m, int columnIndex) {
         double sum = 0;
         for (int i = 0; i < m.length; i++) {
@@ -37,6 +46,7 @@ public class Week3Activity1 {
         return sum;
     }
 
+    // Method for Problem 2
     public static double sumMajorDiagonal(double[][] m) {
         double sum = 0;
         for (int i = 0; i < m.length; i++) {
@@ -44,8 +54,4 @@ public class Week3Activity1 {
         }
         return sum;
     }
-
-    
-        }
-
 }
